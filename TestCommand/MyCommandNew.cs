0cc60c2ed173709e649e85499041d12b41460525
dartlex <dartlex.rmnv@gmail.com>
2026-07-19 22:45:55 +0300

    Ready

---
+++ TestCommand/MyCommandNew.cs
@@ -0,0 +1,22 @@
+﻿using CoursePluginsAPI.cs;
+using NewCoursePlugins.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestCommand
+{
+    public class MyCommandNew : IMyCommand
+
+    {
+        public void Execute(ProjectPageViewModel pageViewModel)
+        {
+            pageViewModel.Elements.Clear();
+            MessageBox.Show("Все элементы удалены");
+
+        }
+    }
+}
