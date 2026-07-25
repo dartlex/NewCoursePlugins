8023ecc9bc0d997f77896a99b6ccb56277498f37
dartlex <dartlex.rmnv@gmail.com>
2026-07-25 22:25:53 +0300

    create lesson9

---
+++ Lessons/Commands/Lesson9.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Commands
+{
+    [Regeneration(RegenerationOption.Manual)]
+    [Transaction(TransactionMode.Manual)]
+    public class Lesson9 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Selection choices = uidoc.Selection;
+
+            Reference hasPickOne = choices.PickObject(ObjectType.Face);
+            if (hasPickOne != null)
+            {
+
+
+            }
+                return Result.Succeeded;
+        }
+    }
+}
